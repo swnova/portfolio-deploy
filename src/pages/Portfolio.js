2cfa0e51302471c07f686bd2f3a9e785be208dc1
swnova <sharrodwillanova@gmail.com>
2022-12-06 03:49:08 -0800

    pj format

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default function Portfolio() {
+    return (
+        <div>
+            <h1>Check out my recent projects!</h1>
+
+
+        </div>    
+    )
+}
---
+++ src/pages/Resume.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h1>Resume</h1>
+      <p>download my resume </p>
+      <p>
+        competencies 
+        -CSS
+        -HTML
+        -JS
+        -bootstrap
+        -MongoDB
+        
+      </p>
+    </div>
+  );
+}
