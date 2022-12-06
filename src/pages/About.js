c4973966ebf60f3277b3c446f7f96ae6616d44df
swnova <sharrodwillanova@gmail.com>
2022-12-06 03:27:14 -0800

    update

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NavTabs from '../components/NavTabs';
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Hello, my name is Sharrod Willanova, I am a recent graduate from the University of Washington's EdX coding bootcamp.
+        I am currently looking for a position as a junior fullstack developer, also I am available for website design and other programming needs.
+
+        Below you will find a few of my recent projects and collaborations.
+        For any other questions you can contact me here<a
+          href="#contact"
+          //  TODO: Add a comment explaining what this logic is doing
+
+
+        ></a>.
+    
+      </p>
+    </div>
+  );
+}
